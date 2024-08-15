c8b6d6a8e373c7ccbda0fb7d5d0009eb8fc41c11
Rich Seviora <richard.seviora@gmail.com>
2024-08-15 10:11:43 -0700

    Clean up SSE

--- apps/api/src/sse/sse.service.ts
+++ apps/api/src/sse/sse.service.ts
@@ -4,10 +4,6 @@ import { Injectable } from '@nestjs/common';
 export interface ISseService {
   emitEvent(id: string, data: EventData): void;
 
-  clearEvent(id: string): void;
-
-  isEventActive(id: string): boolean;
-
   getObservable(id: string): Observable<EventData>;
 }
 
@@ -15,43 +11,15 @@ export interface EventData {
   data: string;
 }
 
-const realId = 'user';
-
 @Injectable()
 export class SseService implements ISseService {
-  private eventSubjects: Map<string, Subject<EventData>> = new Map();
+  private eventSubject: Subject<EventData> = new Subject<EventData>();
 
   emitEvent(id: string, data: EventData) {
-    let eventSubject = this.eventSubjects.get(realId);
-    if (!eventSubject) {
-      eventSubject = new Subject<EventData>();
-      this.eventSubjects.set(realId, eventSubject);
-    }
-    eventSubject.next(data);
+    this.eventSubject.next(data);
   }
 
-  clearEvent(id: string) {
-    const eventSubject = this.eventSubjects.get(id);
-    if (eventSubject) {
-      eventSubject.complete();
-      this.eventSubjects.delete(id);
-    }
-  }
-
-  isEventActive(id: string) {
-    const eventSubject = this.eventSubjects.get(id);
-    if (eventSubject) {
-      return true;
-    }
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars -- temp usage
-  getObservable(eventId: string): Observable<EventData> {
-    let eventSubject = this.eventSubjects.get(realId);
-    if (!eventSubject) {
-      eventSubject = new Subject<EventData>();
-      this.eventSubjects.set(realId, eventSubject);
-    }
-    return eventSubject.asObservable().pipe(map((data) => data));
+  getObservable(): Observable<EventData> {
+    return this.eventSubject.asObservable().pipe(map((data) => data));
   }
 }
