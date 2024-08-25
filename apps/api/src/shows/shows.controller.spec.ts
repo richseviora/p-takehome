38737cac7a938d3e664342c1a5f4076ebd07d294
Rich Seviora <richard.seviora@gmail.com>
2024-08-25 12:09:56 -0700

    Add mocks in for service tests

--- apps/api/src/shows/shows.controller.spec.ts
+++ apps/api/src/shows/shows.controller.spec.ts
@@ -1,15 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
 import { ShowsController } from './shows.controller';
 import { ShowsService } from './shows.service';
 
+const moduleMocker = new ModuleMocker(global);
+
 describe('ShowsController', () => {
   let controller: ShowsController;
+  let showRepositoryMock: any;
 
   beforeEach(async () => {
+    showRepositoryMock = {
+      find: jest.fn(),
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ShowsController],
       providers: [ShowsService],
-    }).compile();
+    })
+      .useMocker((token) => {
+        if (token === 'SHOW_REPOSITORY') {
+          return showRepositoryMock;
+        }
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(
+            token,
+          ) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
 
     controller = module.get<ShowsController>(ShowsController);
   });
--- apps/api/src/shows/shows.service.spec.ts
+++ apps/api/src/shows/shows.service.spec.ts
@@ -1,14 +1,34 @@
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ShowsService } from './shows.service';
 import { showsProviders } from './shows.providers';
 
+const moduleMocker = new ModuleMocker(global);
+
 describe('ShowsService', () => {
   let service: ShowsService;
+  let showRepositoryMock: any;
 
   beforeEach(async () => {
+    showRepositoryMock = {
+      find: jest.fn(),
+    };
     const module: TestingModule = await Test.createTestingModule({
       providers: [ShowsService],
-    }).compile();
+    })
+      .useMocker((token) => {
+        if (token === 'SHOW_REPOSITORY') {
+          return showRepositoryMock;
+        }
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(
+            token,
+          ) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
 
     service = module.get<ShowsService>(ShowsService);
   });
