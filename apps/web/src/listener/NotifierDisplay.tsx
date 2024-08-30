eb6bc497bf354a5efd381bb16d1818bb9edea8e3
Rich Seviora <richard.seviora@gmail.com>
2024-08-30 12:29:21 -0700

    Better UI display

--- apps/web/src/listener/NotifierDisplay.tsx
+++ apps/web/src/listener/NotifierDisplay.tsx
@@ -1,6 +1,7 @@
 import { Fragment, useContext } from "react";
 import { Snackbar } from "@mui/material";
-import { NotifierContext } from "./context.tsx";
+import { Person, Tv, Watch } from "@mui/icons-material";
+import { NotifierContext, SseMessages } from "./context.tsx";
 
 export function NotifierDisplay() {
   const { messages, onClose } = useContext(NotifierContext);
@@ -9,7 +10,7 @@ export function NotifierDisplay() {
       {Object.keys(messages).map((key) => (
         <Notification
           key={key}
-          message={messages[key].data.name}
+          message={messages[key]}
           onClose={() => {
             onClose(key);
           }}
@@ -19,8 +20,38 @@ export function NotifierDisplay() {
   );
 }
 
-export function Notification(props: { message: string; onClose: () => void }) {
+const typeToColorMap: Record<string, string> = {
+  add: "green",
+  updated: "blue",
+  remove: "red",
+};
+
+const typeToIconMap = {
+  user: <Person />,
+  follow: <Watch />,
+  show: <Tv />,
+};
+
+export function Notification(props: {
+  message: SseMessages;
+  onClose: () => void;
+}) {
+  const type = props.message.type;
+  const action = props.message.action;
+  const backgroundColor = typeToColorMap[action];
+  const icon = typeToIconMap[type];
+
   return (
-    <Snackbar message={props.message} onClose={props.onClose} open={true} />
+    <Snackbar
+      onClose={props.onClose}
+      open={true}
+      ContentProps={{ sx: { backgroundColor: backgroundColor } }}
+      message={
+        <>
+          {icon}
+          {type} has been {action}
+        </>
+      }
+    />
   );
 }
