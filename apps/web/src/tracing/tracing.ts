f730e8805ff26eae6afd8ffa79a6f85dcba6febd
Rich Seviora <richard.seviora@gmail.com>
2024-09-01 12:30:01 -0700

    More updates

--- apps/web/src/tracing/tracing.ts
+++ apps/web/src/tracing/tracing.ts
@@ -6,12 +6,16 @@ import {
   SimpleSpanProcessor,
   WebTracerProvider,
 } from "@opentelemetry/sdk-trace-web";
+import { ZoneContextManager } from "@opentelemetry/context-zone";
 import { registerInstrumentations } from "@opentelemetry/instrumentation";
 import { FetchInstrumentation } from "@opentelemetry/instrumentation-fetch";
 
 const logger = debug.debug("app:tracing");
 
 const provider = new WebTracerProvider();
+provider.register({
+  contextManager: new ZoneContextManager(),
+});
 const exporter = new OTLPTraceExporter({
   url: "http://localhost:4318/v1/traces",
 });
